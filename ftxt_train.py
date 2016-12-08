87900b2a8a065f8e0bfc75c62be9098a7604a874
kn45 <381404825@qq.com>
2016-12-08 11:54:22 +0800

    add specific params for large data skipgram

--- ftxt_train.py
+++ ftxt_train.py
@@ -4,10 +4,11 @@ import fasttext
 import sys
 
 
-mode = 'cbow'  # 'cbow' or 'skipgram'
+mode = 'skipgram'  # 'cbow' or 'skipgram'
 input_file = './corpus/ftxt_noclz_corp.seg'
 model_file = 'fasttext_' + mode + '.model'
 
+# Get understood of all the params!!!
 if mode == 'cbow':
     model = fasttext.cbow(
         input_file=input_file,
@@ -19,6 +20,15 @@ if mode == 'cbow':
         thread=12)
 
 if mode == 'skipgram':
-    model = fasttext.cbow(
+    model = fasttext.skipgram(
         input_file=input_file,
-        output=model_file)
+        output=model_file,
+        dim=200,
+        epoch=3,
+        min_count=5,
+        neg=3,
+        word_ngrams=2,
+        bucket=3500000,
+        minn=3,
+        thread=16,
+        silent=0)
